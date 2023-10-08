5c8dce7e70c00635d88af41be7bbdd38262bb5da
cherif <chchch7002@gmail.com>
2023-10-08 21:32:49 +0100

    contact page

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,10 +1,15 @@
+
+
+
+
+
 document.querySelector('.cart-button-container')
     .addEventListener('click',()=>{
         document.querySelector('.cart-container')
             .classList.add('translatx','box-shadow')
         document.querySelector('body').classList.add('stop-scrolling')
     })
-    document.querySelector('.cart-off')
+document.querySelector('.cart-off')
     .addEventListener('click',()=>{
         document.querySelector('.cart-container')
             .classList.remove('translatx','box-shadow')
